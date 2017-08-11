b08d9bad8faa2c9f283065e372b83057bb78308a
w1th3r <MrWitherMail@gmail.com>
2017-08-11 17:58:19 +0300

    asd

--- app.js
+++ app.js
@@ -2,17 +2,8 @@ var express = require('express');
 var app = express();
 var port = process.env.port || 3000;
 
-var request = require('request');
-
 app.listen(port);
 app.get('/', function(req, res)
 {
   res.send("NAHUI!");
-});
-
-var url ='https://requestb.in/18vlicn1?a=1';
-request(url, function (error, response, body) {
-  if (!error) {
-    
-  }
 });
