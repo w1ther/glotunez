093cd532194133bd7bf2c3319912ea06144ef7df
w1th3r <MrWitherMail@gmail.com>
2017-08-11 17:50:43 +0300

    huge changes

--- README.md
+++
@@ -1,2 +0,0 @@
-# glotunez
-GLOT GLOT
--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+var express = require('express');
+var app = express();
+var port = process.env.port || 3000;
+
 var request = require('request');
-var url ='https://requestb.in/18vlicn1';
+
+app.listen(port);
+app.get('/', function(req, res)
+{
+  res.send("NAHUI!");
+});
+
+var url ='https://requestb.in/18vlicn1?a=1';
 request(url, function (error, response, body) {
   if (!error) {
     
