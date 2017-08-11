6c7c27bac701f388fb5512810859f02e87676302
w1th3r <MrWitherMail@gmail.com>
2017-08-11 18:01:14 +0300

    12

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var express = require('express');
 var app = express();
 var port = process.env.port || 3000;
 
-app.listen(port);
 app.get('/', function(req, res)
 {
   res.send("NAHUI!");
 });
+
+app.listen(port);
